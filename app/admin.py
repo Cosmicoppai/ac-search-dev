688e710c3ab5a9fddc2abfef94b7d09fcfedea60
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-10-17 15:48:32 +0530

    updated the admin.py

--- app/admin.py
+++ app/admin.py
@@ -6,12 +6,12 @@ from .models import Post, Comment
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     search_fields = ['comment_id', 'post_id', 'username']
-    list_filter = ['sub', 'username', 'date']
+    list_filter = ['sub', 'username', 'date', 'post_id']
     list_display = ['sub', 'username', 'text']
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    search_fields = ['id', 'username', 'title']
-    list_filter = ['sub', 'username', 'title', 'date']
+    search_fields = ['id', 'username', 'title', 'post_id']
+    list_filter = ['sub', 'username', 'date', ]
     list_display = ['sub', 'username', 'title']
