71b28b8cec15a857d5e4cd6159444480c4426c17
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-10-19 00:35:49 +0530

    added robots.txt

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.staticfiles.storage import staticfiles_storage
+import debug_toolbar
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app.urls")),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    # path("sitemap.xml", sitemap, ),
+    path('__debug__/', include(debug_toolbar.urls)),
 
     ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
