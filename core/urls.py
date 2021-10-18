9645450360cc99a860259d0160f9218ea8b68779
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-10-19 00:59:03 +0530

    added the sitemap.xml

--- core/urls.py
+++ core/urls.py
@@ -3,17 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from django.contrib.sitemaps.views import sitemap
-from django.contrib.staticfiles.storage import staticfiles_storage
-import debug_toolbar
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app.urls")),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
-    # path("sitemap.xml", sitemap, ),
-    path('__debug__/', include(debug_toolbar.urls)),
+    path("sitemap.xml", TemplateView.as_view(template_name="sitemap/sitemap.xml", content_type='application/xml'), name='sitemap'),
 
     ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
