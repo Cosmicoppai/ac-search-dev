3286f34e1c57110dcb01bf01395f89dd390c4c87
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-10-20 15:07:39 +0530

    addde postgres text search

--- app/models.py
+++ app/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 from itertools import chain
 from django.db.models import Q
-# from django.db import connection
 from django.utils.functional import cached_property, keep_lazy
 from django.utils.timezone import now
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchVectorField
+from django.contrib.postgres.indexes import GinIndex
 
 
 class CommentManager(models.Manager):
     def __init__(self):
         super(CommentManager, self).__init__()
-        self.query = "SELECT DISTINCT comment_id, post_id, sub, username, text, upvotes, create_date, delete_date FROM app_comment " \
-                     "WHERE UPPER(text) LIKE UPPER(%s) AND sub=%s ORDER BY create_date DESC"
 
 
     def search(self, search_text, sub):
-        result = self.raw(self.query, (f'%{search_text}%', sub))
-        return result
+        return self.annotate(seach=SearchVector('text')).filter(search=SearchQuery(search_text), sub=sub)
 
     def count_result(self, search_text, sub):
         count = self.filter(Q(text__icontains=search_text),
@@ -32,36 +30,27 @@ class Comment(models.Model):
     upvotes = models.IntegerField(default=0, verbose_name='no of upvotes')
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='date on which comment is created')
     delete_date = models.DateTimeField(blank=True, null=True, verbose_name='date on which comment is deleted')
+    vector_column = SearchVectorField(null=True)
 
     class Meta:
         ordering = ['-create_date']
         verbose_name_plural = "Comment's"
         get_latest_by = ['-create_date']
+        indexes = (GinIndex(fields=['vector_column']),)
 
     def __str__(self):
         return f"{self.sub} -   {self.username}   -   {self.text}"
 
-    """
-    @staticmethod
-    def search(search_text=None):
-        comment_search = CommentSearch(search_text)
-        return comment_search.search()
-        """
-
     objects = CommentManager()
-
 
 
 class PostManager(models.Manager):
     def __init__(self):
         super(PostManager, self).__init__()
-        self.query = "SELECT DISTINCT post_id, sub, username, title, text, upvotes, image_url, " \
-                     "create_date, delete_date FROM app_post WHERE (UPPER(title) LIKE UPPER(%s) OR " \
-                     "UPPER(text) LIKE UPPER(%s)) AND sub=%s ORDER BY create_date DESC"
 
     def search(self, search_text, sub):
-        result = self.raw(self.query, (f'%{search_text}%', f'%{search_text}%', sub))
-        return result
+        return self.filter(Q(title__icontains=search_text) | Q(text__icontains=search_text), Q(sub=sub))
+        # return self.annotate(search=SearchVector('title', 'text', config='english')).filter(search=SearchQuery(search_text), sub=sub)
 
     def count_result(self, search_text, sub):
         count = self.filter(Q(title__icontains=search_text) | Q(text__icontains=search_text), Q(sub__iexact=sub)).count()
@@ -79,21 +68,16 @@ class Post(models.Model):
     image_url = models.URLField(blank=True, null=True, verbose_name="post image url")  # default max_length of 200 is used
     create_date = models.DateTimeField(default=now, verbose_name='date on which post is created')
     delete_date = models.DateTimeField(blank=True, null=True, verbose_name='date on which post is deleted')
+    vector_column = SearchVectorField(null=True)
 
     class Meta:
         ordering = ['-create_date']
         verbose_name_plural = "Post's"
         get_latest_by = ['-create_date']
+        indexes = (GinIndex(fields=['vector_column']), )
 
     def __str__(self):
         return f"{self.sub} -   {self.username}   -   {self.title}"
-
-    """
-    @staticmethod
-    def search(search_text=None):
-        post_search = PostSearch(search_text)
-        return post_search.search()
-        """
 
     objects = PostManager()
 
@@ -107,22 +91,7 @@ class Search:
         self.comment_result = Comment.objects.none()
 
 
-    @keep_lazy(tuple)
-    def search_text(self):
-        if self.filter == 'p':  # if filter is p(posts)
-            self.post_result = Post.objects.search(self.query, self.sub)
-        elif self.filter == 'c':  # if filter is c(comments)
-            self.comment_result = Comment.objects.search(self.query, self.sub)
-        elif self.filter == 'pc':  # if filter is pc(posts and comments)
-            self.post_result = Post.objects.search(self.query, self.sub)
-            self.comment_result = Comment.objects.search(self.query, self.sub)
-        queryset_chain = chain(self.post_result, self.comment_result)
-        # sort the whole queryset in reverse (i.e new post will come first)
-        qs = sorted(queryset_chain, key=lambda instance: instance.create_date, reverse=True)
-        return qs
 
-    """
-    
     def get_model(self):
         if self.filter == 'p':  # if filter is p(posts)
             return Post, None
@@ -131,6 +100,22 @@ class Search:
         if self.filter == 'pc':  # if filter is pc(posts and comments)
             return Post, Comment
 
+
+    @keep_lazy(tuple)
+    def search_text(self):
+        post_model, comment_model = self.get_model()
+        try:
+            self.post_result = post_model.objects.search(self.query, self.sub)
+            self.comment_result = comment_model.objects.search(self.query, self.sub)
+        except AttributeError:
+            pass
+        queryset_chain = chain(self.post_result, self.comment_result)
+        # sort the whole queryset in reverse (i.e new entry will come first)
+        qs = sorted(queryset_chain, key=lambda instance: instance.create_date, reverse=True)
+        return qs
+
+
+    """
     @cached_property
     def count_result(self):
         total_no_of_results = 0
